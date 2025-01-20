32eb2e9200aa9d43df10182fe627c8b2f1dab522
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-01-20 14:19:40 +0500

    Slider moved to separated file

---
+++ resources/views/area_range_slider.blade.php
@@ -0,0 +1,20 @@
+<div class="range_container">
+    <div class="sliders_control">
+        <input id="fromSlider" type="range" value="{{ $area_bounds->min_area }}" min="{{ $area_bounds->min_area }}"
+            max="{{ $area_bounds->max_area }}" />
+        <input id="toSlider" type="range" value="{{ $area_bounds->max_area }}" min="{{ $area_bounds->min_area }}"
+            max="{{ $area_bounds->max_area }}" />
+    </div>
+    <div class="form_control">
+        <div class="form_control_container">
+            <input class="form_control_container__time__input" type="number" id="fromInput"
+                value="{{ $area_bounds->min_area }}" min="{{ $area_bounds->min_area }}"
+                max="{{ $area_bounds->max_area }}" />
+        </div>
+        <div class="form_control_container">
+            <input class="form_control_container__time__input" type="number" id="toInput"
+                value="{{ $area_bounds->max_area }}" min="{{ $area_bounds->min_area }}"
+                max="{{ $area_bounds->max_area }}" />
+        </div>
+    </div>
+</div>
