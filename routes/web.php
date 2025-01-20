ff43a633cf53a1b1638a61b2e92721071b0c6a6d
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-01-20 14:15:26 +0500

    Changed filter route

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 //use Illuminate\Foundation\Application;
 
+use App\Http\Controllers\FilterController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\TireServiceController;
 use App\Models\TireService;
 
-Route::get('/', [HomeController::class, '__invoke'])->name("home");
-
-Route::get('/info', 'App\Http\Controllers\TireServiceController@getService')->name("info");
+Route::get('/', [HomeController::class, "__invoke"])->name("home");
+Route::get('filter', [FilterController::class, "__invoke"])->name("filter");
