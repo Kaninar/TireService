fa24b88c84b876f9038706b37de235336580b6b7
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-01-27 11:43:42 +0500

    deploy.sh added

---
+++ deplay.sh
@@ -0,0 +1,18 @@
+
+composer install
+
+npm install
+npm install bootstrap
+npm install --save vuex-persist
+npm run dev
+
+php artisan migrate
+php artisan db:seed
+
+php artisan key:generate
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Проект задеплоен"
