c764ab2811f36fb3733c95d0e1655fbe4a66b8f9
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-01-27 12:39:19 +0500

    Addition routes added

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,11 @@ use App\Models\TireService;
 
 Route::get('/', [HomeController::class, "__invoke"])->name("home");
 Route::get('filter', [FilterController::class, "__invoke"])->name("filter");
+
+Route::get("/serivces", [TireServiceController::class, "index"])->name("service.index");
+Route::get("/services/create", [TireServiceController::class, "create"])->name("service.create");
+Route::post("/services", [TireServiceController::class, "store"])->name("servcie.store");
+Route::get("/services/{service}", [TireServiceController::class, "show"])->name("service.show");
+Route::get("/services/{service}/edit", [TireServiceController::class, "edit"])->name("serive.edit");
+Route::patch("/services/{service}", [TireServiceController::class, "update"])->name("service.update");
+Route::delete("services/{service}", [TireServiceController::class, "destroy"])->name("service.destroy");
